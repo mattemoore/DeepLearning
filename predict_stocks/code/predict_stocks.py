7408708d3dc3c913e49d042a81f2574a8679c43e
Matt Moore <matt.e.moore@gmail.com>
2017-04-23 09:57:19 -0500

    Basic rolling window linear regression on stock data.

---
+++ predict_stocks/code/predict_stocks.py
@@ -0,0 +1,90 @@
+'''Predict stock market prices, make billions.'''
+
+# pylint: disable=invalid-name
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# load data in numpy array
+STOCK_SYMBOL = 'MSFT'
+ALL_PRICES = pd.read_csv('../input/prices.csv')
+STOCK_PRICES = np.array(ALL_PRICES[ALL_PRICES['symbol'] == STOCK_SYMBOL])
+'''
+ALL_FUNDAMENTALS = pd.read_csv('../input/fundamentals.csv')
+COMPANY_FUNDAMENTALS = \
+    ALL_FUNDAMENTALS[ALL_FUNDAMENTALS['Ticker Symbol'] == STOCK_SYMBOL]
+'''
+
+# csv column indexes
+DATE_COL = 0
+SYMBOL_COL = 1
+OPEN_COL = 2
+CLOSE_COL = 3
+LOW_COL = 4
+HIGH_COL = 5
+VOLUME_COL = 6
+
+# hyper-parameters
+WINDOW_SIZE = 30
+
+# X is matrix of variables and bias term
+X = np.array(
+    STOCK_PRICES[WINDOW_SIZE:, [OPEN_COL, LOW_COL, HIGH_COL, VOLUME_COL]],
+    dtype='float'
+)
+X = np.concatenate((X, np.ones((len(X), 1))), axis=1)
+num_orig_cols = X.shape[1]
+
+# Y is target values
+Y = np.array(
+    STOCK_PRICES[WINDOW_SIZE:, CLOSE_COL],
+    dtype='float'
+)
+
+# create separate array to hold dates
+dates = np.array(
+    STOCK_PRICES[WINDOW_SIZE:, [0]],
+    dtype='datetime64'
+)
+
+# add historical closing prices to X for 'Rolling Window Linear Regression'
+X = np.concatenate(
+    (X, np.zeros((len(X), WINDOW_SIZE))),
+    axis=1
+)
+for row in range(len(X)):
+    for day in range(1, WINDOW_SIZE + 1):
+        col_offset = num_orig_cols - 1 + day
+        row_offset = WINDOW_SIZE + row - day
+        X[row, col_offset] = STOCK_PRICES[row_offset, CLOSE_COL]
+
+# assert X.shape[1] == (WINDOW_SIZE + 5)
+# pd.DataFrame(X).to_csv('X.csv')
+# pd.DataFrame(X).to_csv('Y.csv')
+
+# solve for w
+w = np.linalg.solve(X.T.dot(X), X.T.dot(Y))
+Y_hat = X.dot(w)
+print('w is:', w)
+
+# plot closing price across time
+# convert numpy dates to pandas dates
+pd_dates = []
+for d in range(len(dates)):
+    pd_dates.append(pd.Timestamp(dates[d, 0]))
+plt.scatter(pd_dates, Y)
+plt.plot(pd_dates, Y_hat, color='red')
+plt.show()
+
+
+def get_r_squared(actuals, predicted):
+    '''Calculate r_squared'''
+    d1 = actuals - predicted
+    d2 = actuals - actuals.mean()
+    r_2 = 1 - d1.dot(d1) / d2.dot(d2)
+    print('r_squared is:', r_2)
+
+
+# calculate r_squared
+get_r_squared(Y, Y_hat)
